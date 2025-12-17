af4916d39451464fb8faf304632e828678cbeb4d
Eze-sk <ezequielsmiak@gmail.com>
2025-12-17 18:15:28 -0300

    add the login page

---
+++ src/app/login/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import LoginCard from "@components/LoginCard";
+
+export default function Login() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <LoginCard />
+    </div>
+  );
+}
